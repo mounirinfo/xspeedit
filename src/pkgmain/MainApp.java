cbbeb08747e9ae84991bbe47b3962ed50be74226
mounirinfo <zaghba.mounir@gmail.com>
2017-07-04 14:31:37 +0200

    update shown message

--- src/pkgmain/MainApp.java
+++ src/pkgmain/MainApp.java
@@ -23,30 +23,30 @@ public class MainApp {
 		WrapStrategy currentStrategy = new DefaultStrategy();
 		mainRobot.setStategy(currentStrategy);
 		OutputCartonsChain oCC1= mainRobot.generateOutputChain(inputArticles);
-		System.out.println("1- The output chain is with Default "+oCC1 +" number articles : "+oCC1.getCartons().size());
+		System.out.println("1- The output chain is with Default "+oCC1 +" number of used cartons : "+oCC1.getCartons().size());
 		
 		//2- sort Asc strategy
 		WrapStrategy sortAsc = new SortAscendingBeforeDefault();
 		mainRobot.setStategy(sortAsc);
 		OutputCartonsChain oCC2= mainRobot.generateOutputChain(inputArticles);
-		System.out.println("2- The output chain is with Sort ascending "+oCC2 +" number articles : "+oCC2.getCartons().size());
+		System.out.println("2- The output chain is with Sort ascending "+oCC2 +" number of used cartons : "+oCC2.getCartons().size());
 		
 		//3- sort Desc strategy
 		WrapStrategy sortDesc = new SortDescendingBeforeDefault();
 		mainRobot.setStategy(sortDesc);
 		OutputCartonsChain oCC3= mainRobot.generateOutputChain(inputArticles);
-		System.out.println("3- The output chain is with Sort descending "+oCC3 +" number articles : "+oCC3.getCartons().size());
+		System.out.println("3- The output chain is with Sort descending "+oCC3 +" number of used cartons : "+oCC3.getCartons().size());
 		
 		//4- sort Min Max
 		WrapStrategy sortMinMax = new MaxMinStrategy();
 		mainRobot.setStategy(sortMinMax);
 		OutputCartonsChain oCC4= mainRobot.generateOutputChain(inputArticles);
-		System.out.println("4- The output chain is with Sort Min Max "+oCC4 +" number articles : "+oCC4.getCartons().size());
+		System.out.println("4- The output chain is with Sort Min Max "+oCC4 +" number of used cartons : "+oCC4.getCartons().size());
 		
 		//5- Optimal strategy
 		WrapStrategy optimal = new OptimalSizeStrategy();
 		mainRobot.setStategy(optimal);
 		OutputCartonsChain oCC5= mainRobot.generateOutputChain(inputArticles);
-		System.out.println("5- The output chain is with Optimal Sort "+oCC5 +" number articles : "+oCC5.getCartons().size());
+		System.out.println("5- The output chain is with Optimal Sort "+oCC5 +" number of used cartons : "+oCC5.getCartons().size());
 	}
 }
